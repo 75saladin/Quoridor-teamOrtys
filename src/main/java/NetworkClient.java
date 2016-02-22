f99e28930e9fcb8b58e50df67741266b5eb5cb74
Jed <lechnejc192@potsdam.edu>
2016-02-22 06:42:26 -0500

    Layout.java and Layout2.java added.

--- src/main/java/NetworkClient.java
+++ src/main/java/NetworkClient.java
@@ -5,12 +5,6 @@ import java.io.*;
  *  override handleConnection, but in most cases connect can
  *  remain unchanged. NetworkClient uses SocketUtils to simplify
  *  the creation of the PrintWriter and BufferedReader.
- *  <p>
- *  From <a href="http://courses.coreservlets.com/Course-Materials/">the
- *  coreservlets.com tutorials on JSF 2, PrimeFaces, Ajax, JavaScript, jQuery, GWT, Android,
- *  Spring, Hibernate, JPA, RESTful Web Services, Hadoop, Spring MVC,
- *  servlets, JSP, Java 8 lambdas and streams (for those that know Java already), 
- *  and Java 8 programming (for those new to Java)</a>.
  */
 
 public abstract class NetworkClient {
