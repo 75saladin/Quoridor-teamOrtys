297e892b7f14e7d858d88dda0340bafed7a58a98
Jed <lechnejc192@potsdam.edu>
2016-02-25 21:11:48 -0500

    tests working for layout.java

--- src/test/java/GameClientTest.java
+++ src/test/java/GameClientTest.java
@@ -1,10 +1,7 @@
-import java.io.*;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 public class GameClientTest{
 
