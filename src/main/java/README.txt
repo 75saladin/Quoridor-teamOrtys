05cbd4a6df4bc41570786de48303b94ce0470152
Jed <lechnejc192@potsdam.edu>
2016-02-28 16:38:47 -0500

    ParserTest.java added. Constructor test added.

---
+++ src/main/java/README.txt
@@ -0,0 +1,11 @@
+README
+
+1. Parser class for move and all messages. 
+	- constructor
+	- parse move string method
+	- send to error check
+	
+
+2. Valid move class / server asks this class for all of moves / central move database
+3. TESTS
+4. Broadcast
---
+++ src/test/java/LayoutTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jed_lechner
+ */
+public class LayoutTest {
+    
+    /**
+     * Test of start method, of class Layout.
+     */
+    @Test
+    public void testStart() {
+        System.out.println("start");
+        Stage stage = null;
+        Layout instance = new Layout();
+        //instance.start(stage);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Layout.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        //Layout.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
