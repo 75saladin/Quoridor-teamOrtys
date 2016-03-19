5601f246d668f1512ccee4c88d7d31d97466ffc5
Nicholas Marasco <marascnd197@potsdam.edu>
2016-03-19 16:45:07 -0400

    Added test for new GameClient constructor

--- src/test/java/GameClientTest.java
+++ src/test/java/GameClientTest.java
@@ -5,14 +5,24 @@ import static org.junit.Assert.assertNotNull;
 
 public class GameClientTest{
 
-  // testing GameClient constructor
+  // testing default GameClient constructor
   @Test
-  public void GameClientConstructorTest() throws Exception{
+  public void GameClientDefaultConstructorTest() throws Exception{
     
     GameClient testClient = new GameClient();
 
     assertNotNull("Client created",testClient);
 
+  }
+
+  // testing machine/port pair GameClientConstructor
+  @Test
+  public void GameClientMachinePortPairConstructor() throws Exception{
+    
+    GameClient testClient = new GameClient("localhost",5555);
+
+    assertNotNull("Client created",testClient);
+  
   }
 
   // testing GameClient connection
