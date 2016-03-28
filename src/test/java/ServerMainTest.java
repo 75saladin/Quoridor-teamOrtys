9c909cc9292265dfa6ddfe65e081192f1f8fcb36
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-03-28 10:40:18 -0400

    Added main server class

---
+++ src/test/java/ServerMainTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ServerMainTest {
+
+    private ServerMain main;
+    
+    @Test
+    public void testNetworkStartup(){
+	String[] toPass = new String[2];
+	toPass[0] = "--port";
+	toPass[1] = "4321";
+	main.main(toPass);
+	assertEquals("Main not initialized correctly!", "4321 IAM ORT true", main.getNetwork().toString());
+	main.getNetwork().close();
+    }
+
+}
