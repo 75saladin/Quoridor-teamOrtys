b4718a94da5f7b5e827b427abbd410218039b287
Jed <lechnejc192@potsdam.edu>
2016-04-08 10:26:20 -0400

    Comments added for server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,15 +1,29 @@
 import java.io.*;
 import java.net.*;
 
+/**
+ * 
+ * @author jed_lechner
+ * Abstract class to extend for connection to server. 
+ */
+
 public abstract class Server {
 
-	private int port;
+	private int port; // port to connect to 
 	
+        /**
+         * 
+         * @param port the port to broadcast on
+         * constructor
+         */
 	public Server(int port) {
-		System.out.println("Starting GamerServer on port " + port);
 		this.port = port;
 	}	
 
+        /**
+         * Connect method for clients to connect to. Loop till a client connects.
+         * Server socket closes.
+         */
 	public void connect() {
 		try(ServerSocket socket = new ServerSocket(port)) {
 			Socket client;
@@ -23,5 +37,12 @@ public abstract class Server {
 		}
 	}
 
+        /**
+         * 
+         * @param client Socket the client connects to.
+         * @throws IOException 
+         * 
+         * handles incoming IO
+         */
 	public abstract void handle(Socket client) throws IOException;
 }
