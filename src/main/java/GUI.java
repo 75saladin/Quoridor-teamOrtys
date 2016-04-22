ad76c38b8fd8ffbab0bafca2afa1ed9e56237d5a
Jed <lechnejc192@potsdam.edu>
2016-04-21 21:16:13 -0400

    Changed GUI.java to remove player.

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -218,22 +218,12 @@ public class GUI extends Application implements GUIInterface {
             @Override
             public void run() {
                 grid.getChildren().remove(player.getPlayerNode(num));
-                output.appendText("Kicked player " + num + "\n\n");
-                loserBox.add(player.getPlayerNode(num),num ,num);
-                
-                Text t = new Text("LOSER BOX! HA HA HA");
-                Text t2 = new Text("Heck ya little Rick!");
-                VBox v = new VBox(t, t2, loserBox);
-                root.setRight(v);
-                output.appendText("Ha Ha Ha");
                 player.removePlayer(num);
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(1000);
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
-                
-                player.setPlayerTurn();
             }
         });
         
