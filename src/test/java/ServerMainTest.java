2608f6860c8513f56c8df53083da9150def79b5d
Jed <lechnejc192@potsdam.edu>
2016-04-25 12:33:59 -0400

    Commented out code that isn't needed

--- src/test/java/ServerMainTest.java
+++ src/test/java/ServerMainTest.java
@@ -1,25 +1,25 @@
-import org.junit.Test;
-import org.junit.Ignore;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-
-import java.net.Socket;
-import java.io.PrintWriter;
-import java.io.PrintStream;
-import java.util.Scanner;
-
-public class ServerMainTest {
-
-    private ServerMain main;
-    @Ignore
-    @Test
-    public void testNetworkStartup(){
-	String[] toPass = new String[2];
-	toPass[0] = "--port";
-	toPass[1] = "4321";
-	main.main(toPass);
-	assertEquals("Main not initialized correctly!", "4321 IAM ORT true", main.getNetwork().toString());
-	main.getNetwork().close();
-    }
-
-}
+//import org.junit.Test;
+//import org.junit.Ignore;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertEquals;
+//
+//import java.net.Socket;
+//import java.io.PrintWriter;
+//import java.io.PrintStream;
+//import java.util.Scanner;
+//
+//public class ServerMainTest {
+//
+//    private ServerMain main;
+//    @Ignore
+//    @Test
+//    public void testNetworkStartup(){
+//	String[] toPass = new String[2];
+//	toPass[0] = "--port";
+//	toPass[1] = "4321";
+//	main.main(toPass);
+//	assertEquals("Main not initialized correctly!", "4321 IAM ORT true", main.getNetwork().toString());
+//	main.getNetwork().close();
+//    }
+//
+//}
