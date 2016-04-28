cfac14849e1d8eaccba22f4f43edcff21b693502
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-04-28 17:21:11 -0400

    fixed spacing in GameServerTest

--- src/test/java/GameServerTest.java
+++ src/test/java/GameServerTest.java
@@ -10,81 +10,72 @@ import static org.junit.Assert.*;
 
 public class GameServerTest {
 
-	GameServer server1;
-	GameServer server2;
-	Socket client1;
-	Socket client2;
-	private PrintWriter out1;
-    private BufferedReader in1;
-    private PrintWriter out2;
-    private BufferedReader in2;
+  GameServer server1;
+  GameServer server2;
+  Socket client1;
+  Socket client2;
+  private PrintWriter out1;
+  private BufferedReader in1;
+  private PrintWriter out2;
+  private BufferedReader in2;
 
 
-	@Before
-    public void setUp() throws Exception {
-        server1 = new GameServer(2000, "jed");
-        server2 = new GameServer(2001, "bob");
+  @Before
+  public void setUp() throws Exception {
+    server1 = new GameServer(2000, "jed");
+    server2 = new GameServer(2001, "bob");      
+  }
 
-            
-    }
+  @Test
+  public void testHandle() throws IOException {
+    final String msg1 = "";
+    String msg2 = "";
+    Thread t = new Thread() { 
+      public void handle() {
+        server1.connect();
+      }
+    };
+    t.setDaemon(true);
+    t.start();
 
-    @Test
-    public void testHandle() throws IOException {
-    	final String msg1 = "";
-    	String msg2 = "";
-    	Thread t = new Thread() { 
-    		public void handle() {
-    			server1.connect();
-    		}
-    	};
-    	t.setDaemon(true);
-    	t.start();
-
-    	Thread c1 = new Thread() {
-    		public void handle() {
-    			try {
-    				client1 = new Socket("localhost", 2000);
-    				out1 = new PrintWriter(client1.getOutputStream(), true);
-        			in1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
-    				out1.println("HELLO");
-    				assertEquals(in1.readLine(), "IAM ort:jed");
-    				out1.println("ATARI");
-    			} catch(Exception e) {}
-    		}
-    	};
-    	c1.setDaemon(true);
-    	c1.start();
+    Thread c1 = new Thread() {
+      public void handle() {
+        try {
+          client1 = new Socket("localhost", 2000);
+          out1 = new PrintWriter(client1.getOutputStream(), true);
+          in1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
+          out1.println("HELLO");
+          assertEquals(in1.readLine(), "IAM ort:jed");
+          out1.println("ATARI");
+        } catch(Exception e) {}
+      }
+    };
+    c1.setDaemon(true);
+    c1.start();
 
 
-    	Thread t2 = new Thread() { 
-    		public void handle() {
-    			server2.connect();
-    		}
-    	};
+    Thread t2 = new Thread() { 
+    public void handle() {
+        server2.connect();
+      }
+    };
+    t2.setDaemon(true);
+    t2.start();
 
 
-    	t2.setDaemon(true);
-    	t2.start();
-
-
-    	Thread c2 = new Thread() {
-    		public void handle() {
-    			try {
-    				client2 = new Socket("localhost", 2001);
-    				out2 = new PrintWriter(client2.getOutputStream(), true);
-       	 			in2 = new BufferedReader(new InputStreamReader(client2.getInputStream()));
-    				out2.println("MYOUSHU");
-    				assertNotNull(in2.readLine());
-    				out2.println("ATARI");
-    			} catch(Exception e){}
-    		}
-    	};
-    	c2.setDaemon(true);
-    	c2.start();
-
-
-
-    }
-
-
+    Thread c2 = new Thread() {
+      public void handle() {
+        try {
+          client2 = new Socket("localhost", 2001);
+          out2 = new PrintWriter(client2.getOutputStream(), true);
+          in2 = new BufferedReader(new InputStreamReader(client2.getInputStream()));
+          out2.println("MYOUSHU");
+          assertNotNull(in2.readLine());
+          out2.println("ATARI");
+        } catch(Exception e){}
+      }
+    };
+    c2.setDaemon(true);
+    c2.start();
+  }
 }
