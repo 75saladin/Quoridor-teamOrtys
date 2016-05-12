53dcb1f597270bf39582f7a7a40f779d174699a6
Joshua Naar <naarj197@potsdam.edu>
2016-05-11 21:59:45 -0400

    Removed broken code from EdgeTest.java

--- src/test/java/EdgeTest.java
+++ src/test/java/EdgeTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 public class EdgeTest {
 
     Edge e = new Edge();
-    Edge f = new Edge();
     
-
     @Test
     public void testGetSource() {
 	assertEquals("Testing getSource()", null, e.getSource());
@@ -17,20 +15,5 @@ public class EdgeTest {
     public void testGetTarget() {
 	assertEquals("Testing getTarget()", null, e.getTarget());
     }
-
-  // This test fails and I have no idea why
-  /*  @Test
-    public void testToString() {
-	assertEquals("Testing toString()", "({false, -1,-1}:{false, -1,-1})\n", e.toString());
-    } */ 
-
-  // This test also fails
-  // Fails whether I test it equal to either true or false, which makes absolutely no sense
-	// because e.equals returns a boolean, it has to be true for one of them
-  /*  @Test
-    public void testEquals() {
-	assertEquals("Testing equals()", true, e.equals(f));
-    } */  
-
 
 }
